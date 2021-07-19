57d53ae769acf0296de0ac1e2f29ae24882830a8
zichang <1269653850@qq.com>
2021-07-19 11:56:33 +0800

    a

---
+++ src/test/java/com/lp/un/gshop/GshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.un.gshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
